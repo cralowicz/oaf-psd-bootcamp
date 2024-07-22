424141f4b4837ccb5f112191d838da8e03c907fd
C Ralowicz <cralowicz@wccnet.edu>
2024-07-21 22:15:45 -0400

    "added tests to the test doc"

--- test_is_prime.py
+++ test_is_prime.py
@@ -1,4 +1,15 @@
 #test file for isPrime() in is_prime.py
-import is_prime
+from is_prime import isPrime
+import unittest
 
-
+class TryTesting(unittest.TestCase):
+    def test_is_prime(self):
+        self.assertTrue(isPrime(29))
+    def test_is_prime_2(self):
+       self.assertTrue(isPrime(25)) 
+    def test_is_prime_false(self):
+        self.assertFalse(isPrime(22))
+    def test_is_prime_4(self):
+        self.assertTrue(isPrime('a'))
+if __name__ =="__main__": 
+    unittest.main()
