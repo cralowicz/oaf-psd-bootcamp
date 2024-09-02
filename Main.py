aef082c274f55e64acd39e47d0c4f0ed3e0858b8
C Ralowicz <cralowicz@wccnet.edu>
2024-09-01 21:51:13 -0400

    finished; working program

--- Main.py
+++ Main.py
@@ -1,64 +1,59 @@
 """
+Cosette Ralowicz
+September 2024
+
 a gofundme donation caluclator to figure out what you should donate
 in order for the recipient to receive the full amount intended. Also to convert 
 between currency as needed
  """
 from abc import ABC, abstractmethod
 from datetime import datetime
-from enum import Enum
-import csv
 import sqlite3
 import requests
+import csv
 import json
 from datetime import datetime,timezone
-#class of api calling
-#class for mock data service
-#class for data handler (contain csv, check if needs updating to call api, update db, then query db) 
-
-
-
 
 class AbstractCurrencyService(ABC):
     """barebones of the api calling class and the backup data class"""
     @abstractmethod
+    def get_currency_data(cur, con)->requests:
+        """ open api and update exchange rates in db and datetimes in csv """
+        pass
     def updateDatetimes():
-       """create and/or update the csv"""
+       """update the csv with datetimes"""
     pass
     @abstractmethod
     def compareDateTime():
         """to see if it is time to call api again
-        #compare current datetime (utc) with api's "time of next update" (utc)"""
+        compare current datetime (utc) with "time of next update" (utc) provided by the api which is saved in csv"""
         pass
     @abstractmethod
-    def updateDB():
-        """to see if it is time to call api again
-        #compare current datetime (utc) with api's "time of next update" (utc)"""
+    def getCurrency():
+        """get the user's input as to which currency the donation will be in"""
         pass
-   #should i just open the api in the main method and call methods in response to it opening or nah?
+
     @abstractmethod
-    def openAPI():
-        """to see if it is time to call api again
-        #compare current datetime (utc) with api's "time of next update" (utc)"""
-        pass
-    @abstractmethod
-    def get_rate(currency)->float:
-        """get rate"""
+    def currencyRate(currency, cur):
+        """get the exchange rate for the user inputted currency from the db"""
         pass
     @abstractmethod
     def check_for_currency(currency)->bool:
-        """return false if input is not in db"""
+        """check whether the user inputted currency exists within the db"""
+
 
 class APIService(AbstractCurrencyService):
-    url='https://open.er-api.com/v6/latest/USD'
   
     def get_currency_data(cur, con)->requests:
+        #get api response
         url='https://open.er-api.com/v6/latest/USD'
         response=requests.get(url)
-        #print(type(response))
+        #api success
         if response.status_code==200:
-       # print(response)
             data=response.json()
+  
             rates=data['rates']
+            #update db 
             try:
                 for country in rates.keys():
                     update_query="""UPDATE rates SET rate = ? WHERE currency = ?"""
@@ -69,22 +64,16 @@ class APIService(AbstractCurrencyService):
             except sqlite3.Error as error:
                 print("Failed to update MySQL table {}".format(error))
             res=cur.execute("SELECT * FROM rates")
-            print(res.fetchall())
 
-            #update datetime csv here too
+            #update datetime csv 
             nextUpdate=data['time_next_update_utc']
             last_update=data['time_last_update_utc']
             lastUpdate=datetime.strptime(last_update, '%a, %d %b %Y %X %z')
-            #lastUpdate.strftime('%d %b %Y %X %z')
             update=datetime.strptime(nextUpdate, '%a, %d %b %Y %X %z')
-            #update.strftime('%d %b %Y %X %z') 
-            print(update)
-            print(update.strftime('%d %b %Y %X %z') )
             update_data={'time_last_update_utc':lastUpdate.strftime('%d %b %Y %X %z'),'time_next_update_utc': update.strftime('%d %b %Y %X %z') }
             APIService.updateDatetimes(update_data)
             return response
         else:
-            # print(response)
             return response
         
     def updateDatetimes(update_data):
@@ -94,37 +83,22 @@ class APIService(AbstractCurrencyService):
                  currency_csv.write("%s,%s\n" %(date, update_data[date]))  
                
         currency_csv.close()
-        #update csv
-        pass
+
+#non-api related funcs below
     def compareDateTime()->bool:
-        #NEED TO SAVE DATETIME OF LAST TIME THE DB WAS UPDATED maybe still not sure
-        print("\nReading csv\n")
         with open('C:\\Users\\ckral\\oaf-psd-bootcamp\\currency.csv',mode='r') as file:
             csvFile=csv.reader(file)
             for lines in csvFile:
                 l=lines[0]
                 if (l=="time_next_update_utc"):
                     nextUpdate=lines[1]
-                #if(l=="time_last_update_utc"):
-                 #   lastUpdate=lines[1]
-            
         currentDT = datetime.now(timezone.utc)
         update=datetime.strptime(nextUpdate, '%d %b %Y %X %z')
-       # prevUpdate=datetime.strptime(lastUpdate,'%d %b %Y %X %z')
-        # update=update.strftime('%d %b %Y %X %z')   
-        #if(currentDT>prevUpdate):
-         #   print("\ncurrent time later than last update")
-        #else:
-         #   print("\ncurrent time is before last update...somehow...?")
-        print(currentDT)
-        print(update)
         if(currentDT<update):
             #not time to update
-            print("not yet")
             return False
         elif(update<currentDT):
             #time to update
-            print("time for an update")
             return True
         
     #function to get currency input
@@ -132,10 +106,47 @@ class APIService(AbstractCurrencyService):
         print("Please type in the abbreviation of the currency in which the donation will be received")
         donorCurrency= input()
         donCurrCaps = donorCurrency.upper()
-        print(donCurrCaps)
         return donCurrCaps
     
     #function to return rate
+    def currencyRate(currency, cur):
+    #sql query to get rate inserting currency into where clause
+        res=cur.execute("SELECT rate FROM rates WHERE currency = ?", (currency,))
+        x = res.fetchall()
+        #x is a list of tuples, need to separate out rate as float
+        conversionRate = x[0][0]
+        return conversionRate
+    
+    #function to check if user input currency is in fact in the db
+    def check_for_currency(currency, cur)->bool:
+        #sql query to return a boolean
+        res=cur.execute("SELECT rate FROM rates WHERE currency = ?", (currency,))
+        queryBool = res.fetchone() is not None
+
+        #this if statement handles returning the rate as float
+        if queryBool is True:
+            print (f'Currency is {currency}')
+            return float(APIService.currencyRate(currency,cur))
+
+        #if the currency doesn't exist it gives the user an error
+        else:
+            print ('Currency does not exist.')
+
+
+"""
+Mock Data Handler Class in case API not working
+"""
+class MockDataService(AbstractCurrencyService):
+    #will need to overwrite csv every time
+    def get_currency_data():
+        print("API not connected-out of date exchange rates in use")
+       
+    def getCurrency():
+        print("Please type in the abbreviation of the currency in which the donation will be received")
+        donorCurrency= input()
+        donCurrCaps = donorCurrency.upper()
+        return donCurrCaps
+    
     def currencyRate(dc, cur):
     #sql query to get rate inserting currency into where clause
         res=cur.execute("SELECT rate FROM rates WHERE currency = ?", (dc,))
@@ -158,30 +169,18 @@ class APIService(AbstractCurrencyService):
         #if the currency doesn't exist it gives the user an error
         else:
             print ('Currency does not exist.')
-"""
-Mock Data Handler Class in case API not working
-"""
-class MockDataService(AbstractCurrencyService):
-    #will need to overwrite csv every time
-    def updateDB():
-        print("API not connected-out of date exchange rates in use")
-       
-    def get_rate(currency)->float:
-        """get rate"""
-        pass
-    def check_for_currency(currency)->bool:
-        """return false if input is not in db"""
 
 
     
 """
 Main
 """
-#1)check if update datetime align
-# A)if yes, call api and get response
-#   1)if api response is 200, update the db and csv and use API service
-#   2)if api response is NOT 200 use mock data service (which will just provide un-updated db with warning)
-# B)if no, do not call api and proceed with program
+#1)Check if converting currency
+# 2)check if update datetime align
+#  A)if yes, call api and get response
+#    1)if api response is 200, update the db and csv and use API service
+#    2)if api response is NOT 200 use mock data service (which will just provide un-updated db with warning)
+#  B)if no, do not call api and proceed with program
 
 
 #gofundme fees, 2.9% + $.30usd
@@ -198,7 +197,6 @@ if(converting == "Y" or converting =="y"):
     if(tf==True):
         #call aip
         apiValidation=APIService.get_currency_data(cur, con) #db is now up to date
-        print(apiValidation)
         if(apiValidation.status_code==200):
             print("use api ")
             user_input=APIService.getCurrency()
@@ -206,7 +204,10 @@ if(converting == "Y" or converting =="y"):
             
         else:
             print("use mock")
-            MockDataService.updateDB()
+            MockDataService.get_currency_data()
+            user_input=MockDataService.getCurrency()
+            xchange_rate=MockDataService.check_for_currency(user_input, cur)
+
     elif(tf==False):
         user_input=APIService.getCurrency()
         xchange_rate=APIService.check_for_currency(user_input,cur)
@@ -231,16 +232,18 @@ if(converting == "Y" or converting =="y"):
     floatIntended=float(amountIntended)
 
     amountToDonate=(floatIntended*feePercentage) +(.3*xchange_rate)+floatIntended 
-    amountToDonateUSD=amountToDonate*(xchange_rate) #this turns it into usd
-    print(user_input)
-    print("You need to donate at least", amountToDonate, user_input,", which is ", amountToDonateUSD, "USD")
+    amountToDonateUSD=amountToDonate/(xchange_rate) #this turns it into usd
+    strAtD="{:.2f}".format(amountToDonate)
+    strUSD="{:.2f}".format(amountToDonateUSD)
+    print("You need to donate at least", strAtD, user_input,", which is ", strUSD, "USD")
 
 else:
-#no currency   
+#no currency exchange  
     amountIntended=input("How much do you want the fundee to receive: ")
     floatIntended=float(amountIntended)
     amountToDonate=(floatIntended*feePercentage) +.3+floatIntended
-    print("You need to donate at least", amountToDonate, "USD")
+    strUSD="{:.2f}".format(amountToDonate)
+    print("You need to donate at least", strUSD, "USD")
 
 
 print("End of program")
